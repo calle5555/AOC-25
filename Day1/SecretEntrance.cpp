4d0057dc4f7ecda42a9a11d90bcbe6ec049d33fe
Carl-William Lindkvist <lindkvistcarl01@gmail.com>
2025-12-03 22:12:44 +0100

    Solution for day 1

---
+++ Day1/SecretEntrance.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int task1(int dial_value)
+{
+    int amount_of_zeros = 0;
+    
+    int direction;
+    int steps;
+
+    std::ifstream file("input/task1.txt");
+    std::string str;
+    while(std::getline(file, str))
+    {
+        direction = (str.at(0) == 'L') ? -1 : 1;
+        steps = stoi(str.erase(0,1)) % 100;
+        
+        dial_value = (dial_value + direction * steps) % 100;
+        if(dial_value == 0) amount_of_zeros++;
+    }
+    return amount_of_zeros;
+}
+
+int task2(int dial_value)
+{
+    int amount_of_zeros = 0;
+
+    std::ifstream file("input/task1.txt");
+    std::string str;
+    
+    int direction;
+    int steps;
+    int prev_dial;
+    while(std::getline(file, str))
+    {
+        direction = (str.at(0) == 'L') ? -1 : 1;
+        steps = stoi(str.erase(0,1));
+
+        amount_of_zeros += int(steps/100);
+        steps %= 100;
+
+        prev_dial = dial_value;
+        dial_value += direction * steps;
+        if(dial_value < 0){
+            if(prev_dial != 0) amount_of_zeros++;
+            dial_value += 100;
+        } 
+        else if(dial_value > 100){
+            dial_value = dial_value - 100;
+            amount_of_zeros++;
+        } else if(dial_value == 100){
+            dial_value = dial_value - 100;
+        }
+
+        if(dial_value == 0) amount_of_zeros++;
+    }
+    return amount_of_zeros;
+}
+
+int main()
+{
+    int start_value = 50;
+    std::cout << "Result task 1: " << task1(start_value) << std::endl;
+    std::cout << "Result task 2: " << task2(start_value) << std::endl;
+
+    return 0;
+}
